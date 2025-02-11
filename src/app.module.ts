4f2c3d13f145b2c6eafdb0006b8385a0da2a3fc3
weverton.mata <weverton.mata@outlook.com>
2025-02-10 22:28:12 -0300

    feat: add controllers module with projects, tasks, and users controllers; update app module and eslint configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ControllersModule } from './gateways/controllers/controllers.module';
 
 @Module({
-  imports: [],
+  imports: [ControllersModule],
   controllers: [AppController],
   providers: [AppService],
 })
